195b1bf937472b392585e1e65e8df218dc0302fc
Alfredo Leonelli <alfredoleonellim@gmail.com>
2023-03-10 12:17:07 -0300

    Actualización de controller REST Pokemon

--- src/main/java/com/alfredo/apipokemon/rest/RestPokemonController.java
+++ src/main/java/com/alfredo/apipokemon/rest/RestPokemonController.java
@@ -18,12 +18,13 @@ public class RestPokemonController {
     @Autowired
     private IPokemonRepo pokemonRepo;
 
-    @PostMapping("/pokemon")
+    //Método para guardar un pokémon en la base de datos
+    @PostMapping("/pokemones")
     public Pokemon guardarPokemon(@RequestBody Pokemon pokemon){
-
         return pokemonRepo.save(pokemon);
     }
 
+    //Método que lista todos los pokemones encontrados en la base de datos
     @GetMapping("/pokemones")
     public List<Pokemon> listar(){
         return pokemonRepo.findAll();
